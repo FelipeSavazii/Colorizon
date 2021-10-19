932a31217995385822095dd3678a11567394a66a
FelipeSavazi <90295858+FelipeSavazii@users.noreply.github.com>
2021-10-19 01:54:34 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,16 @@ import re
 
 version = '1.0.0'
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 packages = [
     'colorizon',
 ]
 
 setup(name='colorizon',
       author='Felipe Savazi',
+      author_email="dev@felipesavazi.com",
       url='https://github.com/FelipeSavazii/Colorizon',
       project_urls={
         "Documentation": "https://github.com/FelipeSavazii/Colorizon/tree/main/docs",
@@ -18,6 +22,8 @@ setup(name='colorizon',
       packages=packages,
       license='MIT',
       description='A small library to make it easy to use colors in Python',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       include_package_data=True,
       python_requires='>=3.8.0',
       classifiers=[
@@ -28,10 +34,6 @@ setup(name='colorizon',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
-        'Topic :: Internet',
-        'Topic :: Software Development :: Libraries',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: Utilities',
-        'Typing :: Typed',
+        'Programming Language :: Python :: 3.10',
       ]
 )
