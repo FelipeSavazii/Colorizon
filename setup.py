b377797a5ab899c2c17117a5a174ba9f31169b03
FelipeSavazi <90295858+FelipeSavazii@users.noreply.github.com>
2021-10-19 01:34:16 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,26 +3,6 @@ import re
 
 version = '1.0.0'
 
-if not version:
-    raise RuntimeError('version is not set')
-
-if version.endswith(('a', 'b', 'rc')):
-    # append version identifier based on commit count
-    try:
-        import subprocess
-        p = subprocess.Popen(['git', 'rev-list', '--count', 'HEAD'],
-                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        out, err = p.communicate()
-        if out:
-            version += out.decode('utf-8').strip()
-        p = subprocess.Popen(['git', 'rev-parse', '--short', 'HEAD'],
-                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        out, err = p.communicate()
-        if out:
-            version += '+g' + out.decode('utf-8').strip()
-    except Exception:
-        pass
-
 packages = [
     'colorizon',
     'colorizon.backgrounds',
